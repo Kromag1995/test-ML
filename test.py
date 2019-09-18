5d96feaa73cb1609b039cd7c763cfbefc502fea4
Kromag1995 <gonzalof95@hotmail.com>
2019-09-18 18:14:38 -0300

    Test ML

---
+++ test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from requests import get
+import logging
+
+def reqs(user_id, offset, limit):
+    url = 'https://api.mercadolibre.com/sites/MLA/search?seller_id={user_id}&offset={offset}&limit={limit}'.format(user_id=user_id, offset=offset, limit=limit)
+    response = get(url)
+    return response.json()
+
+def req_items(user_id): 
+    offset= 0
+    rjson = reqs(user_id=user_id, offset=offset, limit=50)
+    items = rjson['results']
+    while rjson['paging']['total'] > offset+50:
+        offset +=50
+        limit = rjson['paging']['total'] - offset
+        if limit >=50:
+            limit = 50
+        rjson = reqs(user_id, offset, limit)
+        items = items+rjson['results']
+    return items
+
+def req_cat_name(cat_id):
+    url = 'https://api.mercadolibre.com/categories/{cat_id}'.format(cat_id=cat_id)
+    response = get(url).json()
+    return response['name']
+
+def logg_item(user_id):
+    items = req_items(user_id)
+    logging.basicConfig(level=logging.INFO,
+                    format='%(message)s',
+                    filename='test_log.log',
+                    filemode='w')
+    for item in items:
+        cat_name = req_cat_name(item['category_id'])
+        logging.info(item['id'] +' , '+ item['title']+' , '+ item['category_id']+' , '+ cat_name)
+
+user_id = 81644614
+logg_item(user_id)
